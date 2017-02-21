65dd0714751199c0e74216aef6bb52510752c38c
Derek Villars and Kory Skarbek <derek+kory@>
2017-02-21 11:06:20 -0800

    add categories and complete by

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -4,141 +4,178 @@ using System;
 
 namespace ToDoList
 {
-  public class Task
-  {
-    private int _id;
-    private string _description;
-
-    public Task(string Description, int Id = 0)
+    public class Task
     {
-      _id = Id;
-      _description = Description;
-    }
+        private int _id;
+        private string _description;
+        private int _categoryId;
+        private string _completeBy;
 
-    public override bool Equals(System.Object otherTask)
-    {
-        if (!(otherTask is Task))
+        public Task(string Description, int CategoryId, string completeBy, int Id = 0)
         {
-          return false;
+            _id = Id;
+            _description = Description;
+            _categoryId = CategoryId;
+            _completeBy = completeBy;
         }
-        else
+
+        public override bool Equals(System.Object otherTask)
         {
-          Task newTask = (Task) otherTask;
-          bool idEquality = (this.GetId() == newTask.GetId());
-          bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
-          return (idEquality && descriptionEquality);
+            if (!(otherTask is Task))
+            {
+                return false;
+            }
+            else
+            {
+                Task newTask = (Task) otherTask;
+                bool idEquality = this.GetId() == newTask.GetId();
+                bool descriptionEquality = this.GetDescription() == newTask.GetDescription();
+                bool categoryEquality = this.GetCategoryId() == newTask.GetCategoryId();
+                return (idEquality && descriptionEquality && categoryEquality);
+            }
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+        public string GetDescription()
+        {
+            return _description;
+        }
+        public void SetDescription(string newDescription)
+        {
+            _description = newDescription;
+        }
+
+        public int GetCategoryId()
+        {
+            return _categoryId;
+        }
+        public void SetCategoryId(int newCategoryId)
+        {
+            _categoryId = newCategoryId;
+        }
+
+        public string GetCompleteBy()
+        {
+            return _completeBy;
+        }
+        public void SetCompleteBy(string newCompleteBy)
+        {
+            _completeBy = newCompleteBy;
+        }
+        public static List<Task> GetAll()
+        {
+            List<Task> AllTasks = new List<Task>{};
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tasks;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                int taskId = rdr.GetInt32(0);
+                string taskDescription = rdr.GetString(1);
+                int taskCategoryId = rdr.GetInt32(2);
+                string taskCompleteBy = rdr.GetString(3);
+                Task newTask = new Task(taskDescription, taskCategoryId, taskCompleteBy, taskId);
+                AllTasks.Add(newTask);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            return AllTasks;
+        }
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO tasks (description, category_id, complete_by) OUTPUT INSERTED.id VALUES (@TaskDescription, @TaskCategoryId, @TaskCompleteBy);", conn);
+
+            SqlParameter descriptionParameter = new SqlParameter();
+            descriptionParameter.ParameterName = "@TaskDescription";
+            descriptionParameter.Value = this.GetDescription();
+
+            SqlParameter categoryIdParameter = new SqlParameter();
+            categoryIdParameter.ParameterName = "@TaskCategoryId";
+            categoryIdParameter.Value = this.GetCategoryId();
+
+            SqlParameter completeByParameter = new SqlParameter();
+            completeByParameter.ParameterName = "@TaskCompleteBy";
+            completeByParameter.Value = this.GetCompleteBy();
+
+            cmd.Parameters.Add(descriptionParameter);
+            cmd.Parameters.Add(categoryIdParameter);
+            cmd.Parameters.Add(completeByParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
+        public static Task Find(int id)
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE id = @TaskId;", conn);
+            SqlParameter taskIdParameter = new SqlParameter();
+            taskIdParameter.ParameterName = "@TaskId";
+            taskIdParameter.Value = id.ToString();
+            cmd.Parameters.Add(taskIdParameter);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            int foundTaskId = 0;
+            string foundTaskDescription = null;
+            int foundTaskCategoryId = 0;
+            string foundCompleteBy = null;
+
+            while(rdr.Read())
+            {
+                foundTaskId = rdr.GetInt32(0);
+                foundTaskDescription = rdr.GetString(1);
+                foundTaskCategoryId = rdr.GetInt32(2);
+                foundCompleteBy = rdr.GetString(3);
+            }
+            Task foundTask = new Task(foundTaskDescription, foundTaskCategoryId, foundCompleteBy, foundTaskId);
+
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            return foundTask;
+        }
+        public static void DeleteAll()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
     }
-
-    public int GetId()
-    {
-      return _id;
-    }
-    public string GetDescription()
-    {
-      return _description;
-    }
-    public void SetDescription(string newDescription)
-    {
-      _description = newDescription;
-    }
-    public static List<Task> GetAll()
-    {
-      List<Task> allTasks = new List<Task>{};
-
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-
-      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks;", conn);
-      SqlDataReader rdr = cmd.ExecuteReader();
-
-      while(rdr.Read())
-      {
-        int taskId = rdr.GetInt32(0);
-        string taskDescription = rdr.GetString(1);
-        Task newTask = new Task(taskDescription, taskId);
-        allTasks.Add(newTask);
-      }
-
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-
-      return allTasks;
-    }
-
-    public void Save()
-    {
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-
-      SqlCommand cmd = new SqlCommand("INSERT INTO tasks (description) OUTPUT INSERTED.id VALUES (@TaskDescription);", conn);
-
-      SqlParameter descriptionParameter = new SqlParameter();
-      descriptionParameter.ParameterName = "@TaskDescription";
-      descriptionParameter.Value = this.GetDescription();
-      cmd.Parameters.Add(descriptionParameter);
-      SqlDataReader rdr = cmd.ExecuteReader();
-
-      while(rdr.Read())
-      {
-        this._id = rdr.GetInt32(0);
-      }
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-    }
-
-    public static Task Find(int id)
-    {
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-
-      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE id = @TaskId;", conn);
-      SqlParameter taskIdParameter = new SqlParameter();
-      taskIdParameter.ParameterName = "@TaskId";
-      taskIdParameter.Value = id.ToString();
-      cmd.Parameters.Add(taskIdParameter);
-      SqlDataReader rdr = cmd.ExecuteReader();
-
-      int foundTaskId = 0;
-      string foundTaskDescription = null;
-      while(rdr.Read())
-      {
-        foundTaskId = rdr.GetInt32(0);
-        foundTaskDescription = rdr.GetString(1);
-      }
-      Task foundTask = new Task(foundTaskDescription, foundTaskId);
-
-      if (rdr != null)
-      {
-        rdr.Close();
-      }
-      if (conn != null)
-      {
-        conn.Close();
-      }
-
-      return foundTask;
-    }
-
-    public static void DeleteAll()
-    {
-      SqlConnection conn = DB.Connection();
-      conn.Open();
-      SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
-      cmd.ExecuteNonQuery();
-      conn.Close();
-    }
-  }
 }
--- Tests/TaskTest.cs
+++ Tests/TaskTest.cs
@@ -6,83 +6,98 @@ using System.Data.SqlClient;
 
 namespace ToDoList
 {
-  public class ToDoTest : IDisposable
-  {
-    public ToDoTest()
+    public class ToDoTest : IDisposable
     {
-      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_test;Integrated Security=SSPI;";
+        public ToDoTest()
+        {
+            DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=todo_test;Integrated Security=SSPI;";
+        }
+
+        [Fact]
+        public void Test_DatabaseEmptyAtFirst()
+        {
+            //Arrange, Act
+            int result = Task.GetAll().Count;
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Test_EqualOverrideTrueForSameDescription()
+        {
+            //Arrange, Act
+            Task firstTask = new Task("Mow the lawn", 1, "05/15/2020");
+            Task secondTask = new Task("Mow the lawn", 1, "05/15/2020");
+
+            //Assert
+            Assert.Equal(firstTask, secondTask);
+        }
+
+        [Fact]
+        public void Test_Save()
+        {
+            //Arrange
+            Task testTask = new Task("Mow the lawn", 1, "05/15/2020");
+            testTask.Save();
+
+            //Act
+            List<Task> result = Task.GetAll();
+            List<Task> testList = new List<Task>{testTask};
+
+            //Assert
+            Assert.Equal(testList, result);
+        }
+
+        [Fact]
+        public void Test_SaveAssignsIdToObject()
+        {
+            //Arrange
+            Task testTask = new Task("Mow the lawn", 1, "05/15/2020");
+            testTask.Save();
+
+            //Act
+            Task savedTask = Task.GetAll()[0];
+
+            int result = savedTask.GetId();
+            int testId = testTask.GetId();
+
+            //Assert
+            Assert.Equal(testId, result);
+        }
+
+        [Fact]
+        public void Test_FindFindsTaskInDatabase()
+        {
+            //Arrange
+            Task testTask = new Task("Mow the lawn", 1, "05/15/2020");
+            testTask.Save();
+
+            //Act
+            Task foundTask = Task.Find(testTask.GetId());
+
+            //Assert
+            Assert.Equal(testTask, foundTask);
+        }
+
+        [Fact]
+        public void Test_SaveCompleteBy()
+        {
+            //Arrange
+            Task testTask = new Task("Mow the lawn", 1, "05/15/2020");
+            testTask.Save();
+
+            //Act
+            List<Task> result = Task.GetAll();
+            List<Task> testList = new List<Task>{testTask};
+
+            //Assert
+            Assert.Equal(testList, result);
+        }
+
+        public void Dispose()
+        {
+            Task.DeleteAll();
+        }
     }
-
-    [Fact]
-    public void Test_DatabaseEmptyAtFirst()
-    {
-     //Arrange, Act
-     int result = Task.GetAll().Count;
-
-     //Assert
-     Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void Test_Equal_ReturnsTrueIfDescriptionsAreTheSame()
-    {
-      //Arrange, Act
-      Task firstTask = new Task("Mow the lawn");
-      Task secondTask = new Task("Mow the lawn");
-
-      //Assert
-      Assert.Equal(firstTask, secondTask);
-    }
-
-    [Fact]
-    public void Test_Save_SavesToDatabase()
-    {
-      //Arrange
-      Task testTask = new Task("Mow the lawn");
-
-      //Act
-      testTask.Save();
-      List<Task> result = Task.GetAll();
-      List<Task> testList = new List<Task>{testTask};
-
-      //Assert
-      Assert.Equal(testList, result);
-    }
-
-    [Fact]
-    public void Test_Save_AssignsIdToObject()
-    {
-      //Arrange
-      Task testTask = new Task("Mow the lawn");
-
-      //Act
-      testTask.Save();
-      Task savedTask = Task.GetAll()[0];
-
-      int result = savedTask.GetId();
-      int testId = testTask.GetId();
-
-      //Assert
-      Assert.Equal(testId, result);
-    }
-
-    [Fact]
-    public void Test_Find_FindsTaskInDatabase()
-    {
-      //Arrange
-      Task testTask = new Task("Mow the lawn");
-      testTask.Save();
-
-      //Act
-      Task foundTask = Task.Find(testTask.GetId());
-
-      //Assert
-      Assert.Equal(testTask, foundTask);
-    }
-
-    public void Dispose()
-    {
-      Task.DeleteAll();
-    }
-  }
 }
